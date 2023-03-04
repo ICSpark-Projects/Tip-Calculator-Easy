1d4cc3a1fc2a1cdd7b8f17caa37f4811d684f451
yhanessaanne <77658704+yhanessaanne@users.noreply.github.com>
2023-03-03 16:18:34 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//Calculate Tip function
+function calculateTip() {
+  //write code here
+}
+
+//Calculate Bill function
+function calculateBill() {
+  //write code here
+}
+
+// Click to call functions
+document.getElementById("calculate").onclick = function() {
+  //write code here
+
+};
+
+  // strech goals: calculate custom tip percentage
+  // strech goals: create error messages for invalid inputs (negative inputs/no inputs)
